e9a8b01da7d28e99bed7e9a87059ddb519abba99
Óscar Manrique <oscar.manrique@vass.es>
2017-11-21 17:55:54 +0100

    test del badge de la celda

--- KataScreenshotTests/SuperHeroTableViewCellTests.swift
+++ KataScreenshotTests/SuperHeroTableViewCellTests.swift
@@ -12,7 +12,31 @@ import XCTest
 
 class SuperHeroTableViewCellTests: ScreenshotTest {
 
+    private let superHeroAvenger = SuperHero(name: "SuperHero - 1",
+        photo: URL(string: ""),
+        isAvenger: true, description: "Description - 1")
+    private let superHeroNoAvenger = SuperHero(name: "SuperHero - 2",
+                                             photo: URL(string: ""),
+                                             isAvenger: false, description: "Description - 2")
+    
     private func givenASuperHeroTableViewCell() -> SuperHeroTableViewCell {
         return UINib(nibName: "SuperHeroTableViewCell", bundle: Bundle.main).instantiate(withOwner: self, options: nil)[0] as! SuperHeroTableViewCell
+    }
+    
+    
+    func testShowBadgeIsSuperHeroIsAvenger(){
+        
+        let cell = givenASuperHeroTableViewCell()
+        cell.configure(forItem: superHeroAvenger)
+        
+        verify(view: cell)
+    }
+    
+    func testShowBadgeIsSuperHeroIsNotAvenger(){
+        
+        let cell = givenASuperHeroTableViewCell()
+        cell.configure(forItem: superHeroNoAvenger)
+        
+        verify(view: cell)
     }
 }
